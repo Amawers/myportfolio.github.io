913c2d2e45917cd3cfa46195063e1c7b22f4f452
Amawers <dacoroon.christianjericho@gmail.com>
2023-02-13 21:36:40 +0800

    added js

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        if(this.scrollY > 200){
+            $('.navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky");
+                }
+
+          // Scrolling Button Btn
+          if(this.scrollY >500){
+            $('.scroll-up-btn').addClass("show");
+            }else{
+            $('.scroll-up-btn').removeClass("show");
+            }   
+    });
+
+    var typed = new Typed(".typing-1",{
+        strings: [
+            "Web Development", "UX/UI Design", "Product Design",
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
